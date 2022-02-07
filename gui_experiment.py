7f6c6844bc79da325c2468fed42f2c93da40c04c
Elizabeth Gardner <egardner23@cornellcollege.edu>
2022-02-06 22:19:16 -0600

    Create Hello World window with label and quit button

--- gui_experiment.py
+++ gui_experiment.py
@@ -2,11 +2,21 @@ __author__ = "Elizabeth Gardner"
 __date__ = "3 February 2022"
 # CSC 512: Professional Practice
 # GUI Experiment
-import tkinter
+
+from tkinter import *
+from tkinter import ttk
 
 
 def main():
-    print("Hello world")
+    root = Tk()
+    root.title("Hello World")
+    frame = ttk.Frame(root, padding=250)
+    frame.grid()
+    ttk.Label(frame, text="Hello World").grid(column=0, row=0)
+    ttk.Button(frame, text="Quit", command=root.destroy).grid(column=1, row=0)
+    root.mainloop()
+
+    # print("Hello world")
 
 
 if __name__ == "__main__":
